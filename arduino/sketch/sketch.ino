71e4418fd9f13f148cdc583db9e619019c314d18
abhishekasharma206 <70856117+abhishekasharma206@users.noreply.github.com>
2021-04-01 13:44:43 +0530

    Voltage sensing

--- arduino/sketch/sketch.ino
+++ arduino/sketch/sketch.ino
@@ -1,3 +1,16 @@
+#include <ArduinoSTL.h>
+std::vector <float> voltages;
+int total_cells = 4;
+
+void voltage_sensing() {
+  // sensing voltage, comapring them with each other and with thresholds
+  for (int pin=97; pin>97-total_cells; pin--)
+  {
+    static int i = 0;
+    voltages[i++] = analogRead(pin);
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
 float min_temp;
@@ -14,5 +27,5 @@ float cellVoltages[99];
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  voltage_sensing();
 }
