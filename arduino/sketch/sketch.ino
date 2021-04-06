e40496da8a1a660f415881a94c0af5292f95ca0c
abhishekasharma206 <70856117+abhishekasharma206@users.noreply.github.com>
2021-04-06 14:44:49 +0530

    Update vector

--- arduino/sketch/sketch.ino
+++ arduino/sketch/sketch.ino
@@ -1,7 +1,10 @@
-#include <Arduino.h>
-#include <ArduinoSTL.h>
+#include<vector.h>
+#include<Pair.h>
+#include<Arduino.h>
+#include<ArduinoSTL.h>
 
-std::vector <float> voltages;
+Vector<Pair<double,int>> voltages;
+typedef Pair<double,int> volt_measurement;
 const int total_cells = 4;
 
 //Variables for current Sensing
@@ -13,17 +16,21 @@ double current = 0;
 double voltage = 0;
 
 //Variables for Temperature sensing
-std::vector<float> temp_sense;
+Vector<Pair<double,int>> temp_sense;
+typedef Pair<double,int> temp_measurement;
 double temp;
 
+//variables for master cut-off
+const int relayPin = 22;
 
 void Temperature_sense(){
   int cell = 0;
-  for(int tempPin = 90 ; tempPin > 90-total_cells ; tempPin--)             // for maximum 6 cells
-    temp_sense.push_back(analogRead(tempPin)*0.48828125);                   // read analog volt from sensor and save to vector temp_sense
+  for(int tempPin = 90 ; tempPin > 90-total_cells ; tempPin--)
+  {             // for maximum 6 cells
+   temp_measurement m(analogRead(tempPin)*0.48828125,tempPin);
+    temp_sense.push_back(m);                   // read analog volt from sensor and save to vector temp_sense
 }                                                                            // convert the analog volt to its temperature equivalent  
-                                                                             // for LM35 IC we have to multiply temperature with 0.48828125
-
+}                                                                             // for LM35 IC we have to multiply temperature with 0.48828125
    /*LM35 sensor has three terminals - Vs, Vout and GND. We will connect the sensor as follows −
 
 Connect the +Vs to +5v on your Arduino board.
@@ -31,9 +38,11 @@ Connect Vout to Analog0 or A0 on Arduino board.
 Connect GND with GND on Arduino.
 
 The Analog to Digital Converter (ADC) converts analog values into a digital approximation based on the formula ADC Value = sample * 1024 / reference voltage (+5v).
-So with a +5 volt reference, the digital approximation will be equal to input voltage * 205.   */    
-                         
-double current_sensing(){
+So with a +5 volt reference, the digital approximation will be equal to input voltage * 205.   */                                                                 
+                                                                         
+
+double current_sensing()
+{
   // senses the current of the battery pack
   adcValue = analogRead(adcVoltage_pin);
   voltage = (adcValue / 1024.0) * 5000; //converts digital value to mV
@@ -41,18 +50,42 @@ double current_sensing(){
 }
 
 
-void voltage_sensing() {
+void voltage_sensing() 
+{
   // sensing voltage of each cell
 
   for (int pin=97; pin>97-total_cells; pin--)               // for maximum 6 cells
   {
-    voltages.push_back(analogRead(pin)*5/1024);
+    volt_measurement m(analogRead(pin)*(5/1024),pin);
+    voltages.push_back(m);
   }
 }
-/* For voltage sensing voltage sensing module is used (VCC<25)
- *  It has 5 pins, VCC - positive terminal to be measured, GND - negative terminal to be measured
- *  S - Analog input to Arduino, +ve - Not Conncted, -ve pin - GND of Arduino
- */
+//turn on the relay
+void turnOn()
+{
+  digitalWrite(relayPin, HIGH);//normally open
+}
+// turn off the relay
+void turnOff()
+{
+  digitalWrite(relayPin, LOW);//normally close
+}
+
+
+
+//charging-discharging
+bool direction_of_flow_of_current()
+{
+  double current=current_sensing();
+  if(current>0)
+  {
+    return 1;
+  }
+  else
+  { 
+    return 0;
+  } 
+}
 
  void Thermal_management(){
   // opens the relay contacts if the temperature is not within the permissible limits
@@ -62,7 +95,7 @@ void voltage_sensing() {
   {
     if (charge = TRUE)
     {
-      if (temp<0 || temp>45)
+      if (temp_sense[i].first<0 || temp_sense[i].first>45)
       {
         digitalWrite(pinout, LOW);
       }
@@ -73,7 +106,7 @@ void voltage_sensing() {
     }
     else
     {
-      if (temp<0 || temp>55)
+      if (temp_sense[i].first<0 || temp_sense[i].first>55)
       {
         digitalWrite(pinout, LOW);
       }
