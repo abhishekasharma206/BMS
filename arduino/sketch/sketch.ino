d483f002b9816aeaf2a5ae1a9d7fe0c384454e10
tushar-git-source <tusharkashikar10@gmail.com>
2021-04-18 04:40:17 +0530

    multiplexer feature added

--- arduino/sketch/sketch.ino
+++ arduino/sketch/sketch.ino
@@ -2,6 +2,14 @@
 #include<Pair.h>
 #include<Arduino.h>
 #include<ArduinoSTL.h>
+
+//select line multiplexer variables
+vector<int> select_line_pins = {93,92,91,90};
+const int current_function_output = 89;       //Pin number for current sensing
+const int temp_function_output = 84;          //Pin number for temperature sensing
+
+
+
 //voltage sensing variable
 Vector<Pair<double,int>> voltages;
 typedef Pair<double,int> volt_measurement;
@@ -10,26 +18,36 @@ const int parallel_cells = 3;
 const int total_cells = series_cells * parallel_cells;
 
 //Variables for current Sensing
-const int adcVoltage_pin = 91; //Pin number for current sensing
-int sensetivity = 185; //As per datasheet of ACS712 for range of 5A
-int adcValue = 0;
-int offsetVoltage = 2500; //(mV) Offset Voltage is Vcc/2. Assuming 5V supply is given through Arduino board.
-double current = 0; 
-double voltage = 0;
+Vector<Pair<double,int>> current_sense;
+typedef Pair<double,int> current_measurement;
+ 
+const int sensetivity = 185;                //As per datasheet of ACS712 for range of 5A
+const int offsetVoltage = 2500;              //(mV) Offset Voltage is Vcc/2. Assuming 5V supply is given through Arduino board.
 
 //Variables for Temperature sensing
 Vector<Pair<double,int>> temp_sense;
 typedef Pair<double,int> temp_measurement;
-double temp;
+
+//working of multiplexer function
+void select_Multiplexer_Pin(byte pin)
+{
+  if (pin > total_cells) return; // Exit the function if it is out of bound
+  for (auto i=0; i<select_line_pins.size(); i++)
+  {
+    if (pin & (1<<i))
+      digitalWrite(select_line_pins[i], HIGH);
+    else
+      digitalWrite(select_line_pins[i], LOW);
+  }
+}
 
 
 void Temperature_sense(){
-  int cell = 0;
-  for(int tempPin = 90 ; tempPin > 90-series_cells ; tempPin--)
-  {             // for maximum 6 cells
-   temp_measurement m(analogRead(tempPin)*0.48828125,tempPin);
-    temp_sense.push_back(m);                   // read analog volt from sensor and save to vector temp_sense
-}                                                                            // convert the analog volt to its temperature equivalent  
+  for(int tempPin = 0 ; tempPin <total_cells) ; tempPin++){
+    select_Multiplexer_Pin(tempPin);
+   temp_measurement m(analogRead(temp_function_output)*0.48828125,tempPin);
+    temp_sense.push_back(m);                                                    // read analog volt from sensor and save to vector temp_sense
+     }                                                                            // convert the analog volt to its temperature equivalent  
 }                                                                             // for LM35 IC we have to multiply temperature with 0.48828125
    /*LM35 sensor has three terminals - Vs, Vout and GND. We will connect the sensor as follows −
 Connect the +Vs to +5v on your Arduino board.
@@ -39,12 +57,18 @@ The Analog to Digital Converter (ADC) converts analog values into a digital appr
 So with a +5 volt reference, the digital approximation will be equal to input voltage * 205.   */                                                                 
                                                                          
 
-double current_sensing()
+void current_sensing()
 {
-  // senses the current of the battery pack
-  adcValue = analogRead(adcVoltage_pin);
-  voltage = (adcValue / 1024.0) * 5000; //converts digital value to mV
-  return ((voltage - offsetVoltage)/sensetivity); //returns the current sensed
+  int raw_voltage = 0;
+  int voltage = 0;
+  
+  for(int cur_Pin = 0 ; cur_Pin < total_cells ; cur_Pin++){
+    select_Multiplexer_Pin(cur_Pin);
+  raw_voltage = (analogRead(current_function_output) / 1024.0) * 5000;            //converts digital value to mV
+  voltage = ((raw_voltage - offsetVoltage)/sensetivity);                             //stores the current sensed in vector
+  current_measurement c(voltage,cur_pin);
+  current_sense.push_back(c);
+  }
 }
 
 
@@ -52,7 +76,7 @@ void voltage_sensing()
 {
   // sensing voltage of each cell
 
-  for (int pin=97; pin > 97-series_cells; pin--)               // for maximum 6 cells
+  for (int pin=97; pin > 97-series_cells; pin--)           
   {
     volt_measurement m(analogRead(pin)*(5/1024),pin);
     voltages.push_back(m);
@@ -116,14 +140,21 @@ bool direction_of_flow_of_current()
 
 void setup() {
 Serial.begin(9600);
-  // put your setup code here, to run once:
 
+ for (auto i=0; i< select_line_pins.size(); i++)
+  {
+    pinMode(select_line_pins[i], OUTPUT);
+    digitalWrite(select_line_pins[i], LOW);
+  }
+  pinMode(current_function_output, OUTPUT); 
+   pinMode(temp_function_output, OUTPUT);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   voltage_sensing();
-  current = current_sensing();
+  current_sensing();
  Temperature_sense();
   Thermal_management();
   cell_balancing();
